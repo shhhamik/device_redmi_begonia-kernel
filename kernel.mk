8a6312543158da9e265c89db6edc06133bf40f7d
DarkJoker360 <simoespo159@gmail.com>
2021-09-08 12:24:11 +0200

    begonia: Drop rootdir scripts
    * We have them already in vendor.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- kernel.mk
+++ kernel.mk
@@ -11,11 +11,3 @@ KERNEL_MODULES_DEST := $(TARGET_COPY_OUT_VENDOR)/lib/modules
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(KERNEL_MODULES_ORIG)/,$(KERNEL_MODULES_DEST))
 
-# Rootdir
-PRODUCT_PACKAGES += \
-    init.bt_drv.rc \
-    init.fmradio_drv.rc \
-    init.gps_drv.rc \
-    init.udc_lib.rc \
-    init.wlan_drv.rc \
-    init.wmt_drv.rc
